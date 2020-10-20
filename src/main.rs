3fd2b1fff30d1c6cea5996bd567f45f07645c47c
Garrett Graves <gravesg57@gmail.com>
2020-10-20 13:17:24 -0400

    basic tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8084").unwrap();
+    println!("Listening on port 8084");
+    for stream in listener.incoming() {
+        println!("Connection recieved");
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
