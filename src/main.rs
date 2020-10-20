ae1799762663022b7a26b0e7196d9a25d265bf22
Garrett Graves <gravesg57@gmail.com>
2020-10-20 14:06:35 -0400

    tcp test

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,27 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::net::UdpSocket;
 fn main() {
-    // let listener = TcpListener::bind("127.0.0.1:8084").unwrap();
-    // println!("Listening on port 8084");
-    // for stream in listener.incoming() {
-    //     println!("Connection recieved");
-    //     let stream = stream.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8084").unwrap();
+    println!("Listening on port 8084");
+    for stream in listener.incoming() {
+        println!("Connection recieved");
+        let stream = stream.unwrap();
 
-    //     handle_connection(stream);
-    // }
+        handle_connection(stream);
+    }
 
-    let socket = UdpSocket::bind("127.0.0.1:8084").expect("couldn't bind to address");
-    println!("UDP Socket on 8084");
-    let mut buf = [0; 10];
-    let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
+    // let socket = UdpSocket::bind("127.0.0.1:8084").expect("couldn't bind to address");
+    // println!("UDP Socket on 8084");
+    // let mut buf = [0; 10];
+    // let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
 
-    println!("{:?}", src_addr);
+    // println!("{:?}", src_addr);
 }
 
-// fn handle_connection(mut stream: TcpStream) {
-//     let mut buffer = [0; 1024];
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
 
-//     stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).unwrap();
 
-//     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-// }
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
