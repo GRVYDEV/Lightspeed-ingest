72d880586f91328ec9a0662992222e3efb88bf22
SNELON <gravesg57@gmail.com>
2020-10-21 21:58:31 -0400

    refactor to use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,29 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::net::UdpSocket;
-use std::thread;
-fn main() {
-    let listener = TcpListener::bind("10.10.0.5:8084").unwrap();
-    println!("Listening on port 8084");
-    for stream in listener.incoming() {
-        println!("Connected");
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 
-        thread::spawn(|| match stream {
-            Ok(stream) => handle_connection(stream),
-            Err(Error) => println!("Error"),
-        });
-        // match stream{
-        //     Ok((socket, addr)) => {
-        //         println!("new client: {:?} stream {:?}", addr, socket);
-        //         handle_connection(socket)
-        //     }
-        //     Err(e) => println!("couldn't get client: {:?}", e),
-        // }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("10.10.0.5:8084").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        handle_connection( socket).await;
     }
-
-    // let socket = UdpSocket::bind("127.0.0.1:8084").expect("couldn't bind to address");
-    // println!("UDP Socket on 8084");
-    // let mut buf = [0; 10];
-    // let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
-
-    // println!("{:?}", src_addr);
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    println!("Sender addr: {:?}", stream.peer_addr());
-    let mut buffer = [0; 1024];
+async fn handle_connection(mut stream: TcpStream) {
+    tokio::spawn(async move {
+        // In a loop, read data from the socket and write the data back.
 
-    stream.read(&mut buffer).unwrap();
+        println!("Sender addr: {:?}", stream.peer_addr());
+        let mut buffer = [0; 1024];
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    println!("Writing");
-    let res = stream.write_all(b"test");
-    match res {
-        Ok(var) => println!("Success {:?}", var),
-        Err(err) => println!("there was an error {:?}", err),
-    }
-
-    let mut buffer2 = [0; 1024];
-
-    stream.read(&mut buffer2).unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer2[..]));
+        match stream.read(&mut buffer).await {
+            Ok(var) => println!("Success {:?}", var),
+            Err(err) => println!("there was an error {:?}", err),
+        };
+    });
 }
