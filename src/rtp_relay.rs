3b49f150a589eaf980ce6c1494322b2b626b354b
SNELON <gravesg57@gmail.com>
2020-12-20 00:26:24 -0500

    Implement dot command

--- src/rtp_relay.rs
+++ src/rtp_relay.rs
@@ -4,10 +4,11 @@ use hex::{decode, encode};
 use rand::distributions::Uniform;
 use rand::{thread_rng, Rng};
 use ring::hmac;
+use std::io;
 use tokio::net::UdpSocket;
 use tokio::sync::mpsc;
+use tokio_util::codec::BytesCodec;
 use tokio_util::codec::Framed;
-
 pub struct UdpConnection {}
 
 #[derive(Debug)]
@@ -23,13 +24,24 @@ impl UdpConnection {
             let recv_socket = UdpSocket::bind(format!("0.0.0.0:{}", recv_socket_port))
                 .await
                 .expect("Failed to bind to port");
-                
+
+            match recv_socket
+                .connect(format!("0.0.0.0:{}", recv_socket_port))
+                .await
+            {
+                Ok(_) => {}
+                Err(e) => println!("There was an error connecting to udp socket {:?}", e),
+            };
             loop {
+                match recv_socket.readable().await {
+                    Ok(_) => {}
+                    Err(e) => println!("Error waiting for socket to be readable {:?}", e),
+                };
                 let mut buf = [0 as u8];
-                println!("Connected to udp socket");
-                match recv_socket.recv(&mut buf).await {
-                    Ok(_) => {
-                        println!("Receieved");
+
+                match recv_socket.try_recv(&mut buf) {
+                    Ok(n) => {
+                        println!("Receieved {:?} bytes", n);
                         match relay_send
                             .send(UdpRelayCommand::Send { data: buf.to_vec() })
                             .await
@@ -39,6 +51,9 @@ impl UdpConnection {
                                 println!("There was an error sending the packet to the relay task. Error: {:?}", e)
                             }
                         }
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        continue;
                     }
                     Err(e) => {
                         println!("There was an error reading from the socket. Error: {:?}", e)
@@ -52,19 +67,23 @@ impl UdpConnection {
                 .await
                 .expect("failed to bind to 127.0.0.1:9000");
 
+            match send_socket.connect("127.0.0.1:9000").await {
+                Ok(_) => {}
+                Err(e) => println!("There was an error connecting to udp socket {:?}", e),
+            }
             loop {
                 match relay_receive.recv().await {
                     Some(UdpRelayCommand::Send { data }) => {
                         println!("Received UDP RELAY COMMAND");
-                        // match send_socket.send(data.as_slice()).await {
-                        //     Ok(_) => {}
-                        //     Err(e) => {
-                        //         println!(
-                        //             "Failed to send packet to loopback interface. Error: {:?}",
-                        //             e
-                        //         )
-                        //     }
-                        // };
+                        match send_socket.send(data.as_slice()).await {
+                            Ok(_) => {}
+                            Err(e) => {
+                                println!(
+                                    "Failed to send packet to loopback interface. Error: {:?}",
+                                    e
+                                )
+                            }
+                        };
                     }
                     Some(UdpRelayCommand::Kill) => {}
                     None => {}
