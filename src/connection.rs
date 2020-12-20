a9fcc30521cda058401fe3069c6c9d2a9afbd869
SNELON <gravesg57@gmail.com>
2020-12-19 23:38:15 -0500

    Implement dot command

--- src/connection.rs
+++ src/connection.rs
@@ -301,6 +301,7 @@ async fn handle_command(
             }
         }
         FtlCommand::Ping => {
+            println!("Handling HMAC Command");
             resp = Vec::new();
             resp.push("201\n".to_string());
             match sender.send(FrameCommand::Send { data: resp }).await {
