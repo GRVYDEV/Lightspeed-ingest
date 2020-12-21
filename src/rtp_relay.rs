83eadcbf50f43ff4dc66d69c83485ac32a1778fe
SNELON <gravesg57@gmail.com>
2020-12-21 17:39:19 -0500

    Make udp tasks abortable

--- src/rtp_relay.rs
+++ src/rtp_relay.rs
@@ -32,9 +32,9 @@ pub async fn real_receive_start(
     let recv_socket = UdpSocket::bind("10.17.0.5:65535")
         .await
         .expect("Failed to bind to port");
-    let mut bytes = vec![0 as u8; 4096];
+    let mut bytes ;
     loop {
-        
+            bytes = vec![0 as u8; 4096];
         // let mut buf = [0 as u8; 2000];
         match recv_socket.recv(&mut bytes).await {
             Ok(n) => {
@@ -46,7 +46,6 @@ pub async fn real_receive_start(
                     data: bytes[..n].to_vec(),
                 }) {
                     Ok(_) => {
-                        bytes.clear();
                     }
                     Err(e) => {
                         println!(
